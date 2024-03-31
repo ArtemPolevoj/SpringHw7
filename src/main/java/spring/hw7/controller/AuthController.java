ccd97c67e7603eaedeb7c4563293006dcd6a9390
artem <artempolevoj@yandex.ru>
2024-03-31 13:38:34 +0300

    add AuthController

--- src/main/java/spring/hw7/controller/AuthController.java
+++ src/main/java/spring/hw7/controller/AuthController.java
@@ -3,11 +3,7 @@ package spring.hw7.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import spring.hw7.model.Person;
 import spring.hw7.service.PersonService;
 
 
