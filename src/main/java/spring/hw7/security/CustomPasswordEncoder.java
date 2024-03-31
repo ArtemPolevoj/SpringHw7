341023df9b20b61ecaa3467416dcfe42ba8ae01f
artem <artempolevoj@yandex.ru>
2024-03-31 13:32:57 +0300

    add CustomPasswordEncoder

---
+++ src/main/java/spring/hw7/security/CustomPasswordEncoder.java
@@ -0,0 +1,21 @@
+package spring.hw7.security;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class CustomPasswordEncoder implements PasswordEncoder {
+
+
+    @Override
+    public String encode(CharSequence rawPassword) {
+       // return new BCryptPasswordEncoder().toString();
+        return String.valueOf(rawPassword);
+    }
+
+    @Override
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return encode(rawPassword).equals(encodedPassword);
+    }
+}
